917e19788ce2c20137a883bb980455a44bd164a8
kylewelt <kylewelt@umich.edu>
2017-09-06 15:48:03 -0400

    calculate and display percent positive months

--- parse_fund_data.js
+++ parse_fund_data.js
@@ -28,14 +28,28 @@ function getCumulativeReturn (returns) {
   return cumulative_return;
 }
 
+function getPercentPositive (returns) {
+  let pos_count = 0;
+
+  for (let i = 0; i < returns.length; i++) {
+    if (Number(returns[i]) > 0) {
+      pos_count += 1;
+    }
+  }
+
+  return (pos_count / returns.length * 100);
+}
+
 function main () {
   let fund_name = readLine();
   let fund_year = readLine();
   let fund_returns = readLine().split(', ');
 
   let cumulative_return = getCumulativeReturn(fund_returns);
+  let percent_positive = getPercentPositive(fund_returns);
 
   console.log(fund_name);
   console.log(`Year: ${fund_year}`);
   console.log(`Cumulative Return: ${cumulative_return.toFixed(2)}%`);
+  console.log(`% Positive Months: ${percent_positive.toFixed(2)}%`);
 }
