e1cf2b3f836486afe896dd7c2d640c1336cf0302
kylewelt <kylewelt@umich.edu>
2017-09-06 15:29:29 -0400

    calculate cumulative return

--- parse_fund_data.js
+++ parse_fund_data.js
@@ -20,8 +20,22 @@ function readLine () {
 
 /////////////// process stdin above this line ////////////////////
 
+function getCumulativeReturn (returns) {
+  let cumulative_return = returns.reduce(function (acc, value) {
+    return acc + Number(value);
+  }, 0);
+
+  return cumulative_return;
+}
+
 function main () {
   let fund_name = readLine();
   let fund_year = readLine();
   let fund_returns = readLine().split(', ');
+
+  let cumulative_return = getCumulativeReturn(fund_returns);
+
+  console.log(fund_name);
+  console.log(`Year: ${fund_year}`);
+  console.log(`Cumulative Return: ${cumulative_return.toFixed(2)}%`);
 }
