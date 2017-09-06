e11224ce2c845a51cfa97e2351d135e93f2b2208
kylewelt <kylewelt@umich.edu>
2017-09-06 16:06:00 -0400

    find and display best monthly return, made return conversion to number DRY

--- parse_fund_data.js
+++ parse_fund_data.js
@@ -20,9 +20,17 @@ function readLine () {
 
 /////////////// process stdin above this line ////////////////////
 
+function arrayValuesToNumbers (array) {
+  let array_numbers = array.map(function (value) {
+    return Number(value);
+  });
+
+  return array_numbers;
+}
+
 function getCumulativeReturn (returns) {
   let cumulative_return = returns.reduce(function (acc, value) {
-    return acc + Number(value);
+    return acc + value;
   }, 0);
 
   return cumulative_return;
@@ -32,7 +40,7 @@ function getPercentPositive (returns) {
   let pos_count = 0;
 
   for (let i = 0; i < returns.length; i++) {
-    if (Number(returns[i]) > 0) {
+    if (returns[i] > 0) {
       pos_count += 1;
     }
   }
@@ -40,16 +48,33 @@ function getPercentPositive (returns) {
   return (pos_count / returns.length * 100);
 }
 
+function getBestMonth (returns) {
+  let best_month = returns[0];
+
+  for (let i = 0; i < returns.length; i++) {
+    if (returns[i] > best_month) {
+      best_month = returns[i];
+    }
+  }
+
+  return best_month;
+}
+
 function main () {
   let fund_name = readLine();
   let fund_year = readLine();
-  let fund_returns = readLine().split(', ');
+  let fund_return_strings = readLine().split(', ');
+  let fund_returns = arrayValuesToNumbers(fund_return_strings);
 
   let cumulative_return = getCumulativeReturn(fund_returns);
   let percent_positive = getPercentPositive(fund_returns);
+  let best_month = getBestMonth(fund_returns);
 
   console.log(fund_name);
   console.log(`Year: ${fund_year}`);
   console.log(`Cumulative Return: ${cumulative_return.toFixed(2)}%`);
   console.log(`% Positive Months: ${percent_positive.toFixed(2)}%`);
+  console.log(`Best Month: ${best_month.toFixed(2)}%`);
+
+  return 0;
 }
